088d15284d6e41e62d684f9231009f571c7534a3
misacha775 <misatya1995@gmail.com>
2026-01-20 11:47:46 +0900

    Use SchoolGrade model in Student ShowController

--- app/Http/Controllers/Student/ShowController.php
+++ app/Http/Controllers/Student/ShowController.php
@@ -5,14 +5,13 @@ namespace App\Http\Controllers\Student;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Student;
-use Illuminate\Support\Facades\DB;
+use App\SchoolGrade;
 
 class ShowController extends Controller
 {
     public function __invoke(Request $request, Student $student)
     {
-        $query = DB::table('school_grades')
-            ->where('student_id', $student->id);
+        $query = SchoolGrade::where('student_id', $student->id);
 
         if ($request->filled('grade')) {
             $query->where('grade', $request->grade);
